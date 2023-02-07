0f26cdd0eea56daff20bb99f0d90a3c050991a0f
Hicks-99 <94490389+Hicks-99@users.noreply.github.com>
2023-02-07 10:30:23 +0100

    Switched to Cloudflare Adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import json from '@rollup/plugin-json';
 
